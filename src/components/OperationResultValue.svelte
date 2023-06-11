97f3b50330f2b49106d3b86108f6002c33edcb95
exertieb <bastien.exertier@grenoble-inp.org>
2023-06-11 13:33:51 +0900

    shows ... instead of [object Object] in the results

--- src/components/OperationResultValue.svelte
+++ src/components/OperationResultValue.svelte
@@ -2,6 +2,7 @@
 	import XLg from "svelte-bootstrap-icons/lib/XLg.svelte";
 	import Check2 from "svelte-bootstrap-icons/lib/Check2.svelte";
 	import DashLg from "svelte-bootstrap-icons/lib/DashLg.svelte";
+	import ThreeDots from "svelte-bootstrap-icons/lib/ThreeDots.svelte";
 
 	export let property;
 	export let value;
@@ -11,6 +12,8 @@
 	{#if value}<Check2/>{:else}<XLg/>{/if}
 {:else if value === null}
 	<DashLg/>
+{:else if typeof value === "object"}
+	<ThreeDots/>
 {:else}
 	{value}
 {/if}
