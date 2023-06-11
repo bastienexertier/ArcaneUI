8e1ec787085d2fd90dcbfd77360aae58dd292861
exertieb <bastien.exertier@grenoble-inp.org>
2023-06-11 12:49:02 +0900

    improved anyOf

--- src/components/forms/OperationFormAnyOf.svelte
+++ src/components/forms/OperationFormAnyOf.svelte
@@ -6,19 +6,22 @@
 	export let currentId;
 
 	let selected = null;
+	const handleDelete = () => selected = null;
 </script>
 
-<div class="d-flex flex-row choices">
-	{#each schemas as schema, index}
-		<div class="me-2" class:selected={index === selected} on:click={() => selected = index}>
-			{schema.title || (schema.items && schema.items.title) || schema.type}
-		</div>
-	{/each}
-</div>
+{#if selected === null}<!-- TODO make sure one is selected when submitting -->
+	<div class="d-flex flex-row choices">
+		{#each schemas as schema, index}
+			<div class="me-2" class:selected={index === selected} on:click={() => selected = index}>
+				{schema.title || (schema.items && schema.items.title) || schema.type}
+			</div>
+		{/each}
+	</div>
+{/if}
 
 {#key selected}
 	{#if selected !== null}
-		<OperationFormBase {operationId} schema={schemas[selected]} {currentId} />
+		<OperationFormBase {operationId} schema={schemas[selected]} {currentId} {handleDelete} required/>
 		<input type="hidden" value={selected} name={currentId + '.anyOf'}/>
 	{/if}
 {/key}
