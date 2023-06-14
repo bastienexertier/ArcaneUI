3bc688c1e0310b57b400b92c93497aaef5753d1d
exertieb <bastien.exertier@grenoble-inp.org>
2023-06-14 17:58:04 +0900

    refactored input selector

--- src/components/forms/OperationFormInput.svelte
+++ src/components/forms/OperationFormInput.svelte
@@ -1,9 +1,5 @@
 <script>
-	import { getInputType } from '/src/lib.js';
-
-	import DefaultInput from './inputs/DefaultInput.svelte';
-	import BooleanInput from './inputs/BooleanInput.svelte';
-	import EnumInput from './inputs/EnumInput.svelte';
+	import InputSelector from './inputs/InputSelector.svelte';
 
 	export let id;
 	export let name;
@@ -13,18 +9,10 @@
 	let inputId = `${id}_${name}`;
 
 	let title = schema.title || schema.name || name.split('.').slice(-1);// || parameter.name;
-	let inputType = getInputType(schema);
 </script>
 
-
 <label for={inputId}>{title}</label>
-{#if schema.enum}
-	<EnumInput {inputId} {name} {required} enumValues={schema.enum}/>
-{:else if schema.type == 'boolean'}
-	<BooleanInput {inputId} {name} defaultValue={schema.default}/>
-{:else}
-	<DefaultInput {inputId} {name} {required} {inputType} defaultValue={schema.default}/>
-{/if}
+<InputSelector {inputId} {schema} {name} {required} />
 {#if schema.description}
 	<small>{schema.description}</small>
 {/if}
---
+++ src/components/forms/inputs/InputSelector.svelte
@@ -0,0 +1,22 @@
+<script>
+	import { getInputType } from '/src/lib.js';
+
+	import DefaultInput from './DefaultInput.svelte';
+	import BooleanInput from './BooleanInput.svelte';
+	import EnumInput from './EnumInput.svelte';
+
+	export let inputId;
+	export let schema;
+	export let name;
+	export let required;
+
+	let inputType = getInputType(schema);
+</script>
+
+{#if schema.enum}
+	<EnumInput {inputId} {name} {required} enumValues={schema.enum}/>
+{:else if schema.type == 'boolean'}
+	<BooleanInput {inputId} {name} defaultValue={schema.default}/>
+{:else}
+	<DefaultInput {inputId} {name} {required} {inputType} defaultValue={schema.default}/>
+{/if}
