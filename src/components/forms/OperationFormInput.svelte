f2d117f4fcfd02afc83e52eb3f484e79ed10d3b5
exertieb <bastien.exertier@grenoble-inp.org>
2023-06-14 17:54:45 +0900

    renamed input components

--- src/components/forms/OperationFormInput.svelte
+++ src/components/forms/OperationFormInput.svelte
@@ -1,9 +1,9 @@
 <script>
 	import { getInputType } from '/src/lib.js';
 
-	import OperationFormInputBase from './inputs/OperationFormInputBase.svelte';
-	import OperationFormInputSelect from './inputs/OperationFormInputSelect.svelte';
-	import OperationFormInputCheckbox from './inputs/OperationFormInputCheckbox.svelte';
+	import DefaultInput from './inputs/DefaultInput.svelte';
+	import BooleanInput from './inputs/BooleanInput.svelte';
+	import EnumInput from './inputs/EnumInput.svelte';
 
 	export let id;
 	export let name;
@@ -13,20 +13,17 @@
 	let inputId = `${id}_${name}`;
 
 	let title = schema.title || schema.name || name.split('.').slice(-1);// || parameter.name;
-	//let required = schema.required || !('default' in schema);
 	let inputType = getInputType(schema);
-
-	//console.log(id, name, schema, required);
 </script>
 
 
 <label for={inputId}>{title}</label>
 {#if schema.enum}
-	<OperationFormInputSelect {inputId} {name} {required} enumValues={schema.enum}/>
+	<EnumInput {inputId} {name} {required} enumValues={schema.enum}/>
 {:else if schema.type == 'boolean'}
-	<OperationFormInputCheckbox {inputId} {name} defaultValue={schema.default}/>
+	<BooleanInput {inputId} {name} defaultValue={schema.default}/>
 {:else}
-	<OperationFormInputBase {inputId} {name} {required} {inputType} defaultValue={schema.default}/>
+	<DefaultInput {inputId} {name} {required} {inputType} defaultValue={schema.default}/>
 {/if}
 {#if schema.description}
 	<small>{schema.description}</small>
